a0cb07e10a0bcce74dd03554997eccf797b096d3
Natha R. Cervieri <nathan.reikdalc@gmail.com>
2020-03-28 16:23:28 -0300

    Base do frontend feita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TelaPrincipalComponent } from './tela-principal/tela-principal.component';
-import { BarraStatusComponent } from './barra-status/barra-status.component';
-import { AreaMensagemComponent } from './area-mensagem/area-mensagem.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
-import { MessageAresComponent } from './message-ares/message-ares.component';
 import { MessageAreaComponent } from './message-area/message-area.component';
 import { MainEditComponent } from './main-edit/main-edit.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TelaPrincipalComponent,
-    BarraStatusComponent,
-    AreaMensagemComponent,
     StatusBarComponent,
-    MessageAresComponent,
     MessageAreaComponent,
-    MainEditComponent
+    MainEditComponent,
+    MainScreenComponent,
+    ToolBarComponent
   ],
   imports: [
     BrowserModule,
