092b792b848c1c6fde308a45dd5c2db5acfba6b3
Natha R. Cervieri <nathan.reikdalc@gmail.com>
2020-03-28 16:02:09 -0300

    Criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaPrincipalComponent } from './tela-principal/tela-principal.component';
+import { BarraStatusComponent } from './barra-status/barra-status.component';
+import { AreaMensagemComponent } from './area-mensagem/area-mensagem.component';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { MessageAresComponent } from './message-ares/message-ares.component';
+import { MessageAreaComponent } from './message-area/message-area.component';
+import { MainEditComponent } from './main-edit/main-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaPrincipalComponent,
+    BarraStatusComponent,
+    AreaMensagemComponent,
+    StatusBarComponent,
+    MessageAresComponent,
+    MessageAreaComponent,
+    MainEditComponent
   ],
   imports: [
     BrowserModule,
